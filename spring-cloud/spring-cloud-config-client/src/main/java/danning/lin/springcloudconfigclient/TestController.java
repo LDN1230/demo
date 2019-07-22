a701ee97afa44875ee93e07b432b28017931905d
Danny Lin <32321924+LDN1230@users.noreply.github.com>
2019-07-22 09:34:34 +0800

    spring cloud config client加了refresh功能，包括远程git仓库，本地git仓库和本地文件系统配置

--- spring-cloud/spring-cloud-config-client/src/main/java/danning/lin/springcloudconfigclient/TestController.java
+++ spring-cloud/spring-cloud-config-client/src/main/java/danning/lin/springcloudconfigclient/TestController.java
@@ -1,4 +1,19 @@
 package danning.lin.springcloudconfigclient;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
 public class TestController {
+
+    @Value("${name:fail}")//如果读不到配置文件的name,就name=fail
+    private String name;
+
+    @RequestMapping("/name")
+    public String returnName(){
+        return name;
+    }
 }
